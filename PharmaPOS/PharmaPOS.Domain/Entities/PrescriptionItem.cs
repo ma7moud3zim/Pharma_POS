f7ff8555ca1638056311b26cffc4ab9635bef5ae
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-08-16 03:43:13 +0300

    created Prescription and Prescription Item entities

---
+++ PharmaPOS/PharmaPOS.Domain/Entities/PrescriptionItem.cs
@@ -0,0 +1,22 @@
+﻿using PharmaPOS.Domain.Common;
+
+namespace PharmaPOS.Domain.Entities;
+
+public class PrescriptionItem : BaseEntity
+{
+    public int PrescribedQuantity { get; set; }
+    public int? DispensedQuantity { get; set; }
+    public string? DrugNameAsWritten { get; set; }
+    public string? Dosage { get; set; }
+    public string? Duration { get; set; }
+    public string? Instructions { get; set; }
+    public bool IsSubstitutionAllowed { get; set; } = false;
+
+    // Foreign Keys
+    public Guid PrescriptionId { get; set; }
+    public Guid DrugId { get; set; }
+
+    // Navigation Properties
+    public Prescription Prescription { get; set; } = null!;
+    public Drug Drug { get; set; } = null!;
+}
