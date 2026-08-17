3df6a0239b6b4f4165c44ad7e6ecfb209990b4ff
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-08-17 08:06:27 +0300

    Created Payment configurations files

---
+++ PharmaPOS/PharmaPOS.Infrastructure/Data/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaPOS.Domain.Entities;
+
+namespace PharmaPOS.Infrastructure.Data.Configurations;
+
+public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+{
+    public void Configure(EntityTypeBuilder<Payment> builder)
+    {
+        builder.ToTable("Payments");
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Amount)
+            .HasColumnType("decimal(18,4)");
+
+        builder.Property(p => p.Method)
+            .HasConversion<string>()
+            .HasMaxLength(30);
+
+        builder.Property(p => p.ReferenceNumber)
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Notes)
+            .HasMaxLength(500);
+
+        builder.HasIndex(p => p.SaleId)
+            .HasDatabaseName("IX_Payments_SaleId");
+
+        builder.HasOne(p => p.Sale)
+            .WithMany(s => s.Payments)
+            .HasForeignKey(p => p.SaleId)
+            .OnDelete(DeleteBehavior.ClientNoAction);
+    }
+}
---
+++ PharmaPOS/PharmaPOS.Infrastructure/Data/Configurations/PaymentConfigurationItem.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmaPOS.Domain.Entities;
+
+namespace PharmaPOS.Infrastructure.Data.Configurations;
+
+public class PurchaseOrderConfiguration : IEntityTypeConfiguration<PurchaseOrder>
+{
+    public void Configure(EntityTypeBuilder<PurchaseOrder> builder)
+    {
+        builder.ToTable("PurchaseOrders");
+
+        builder.HasKey(po => po.Id);
+
+        builder.Property(po => po.OrderNumber)
+            .IsRequired()
+            .HasMaxLength(30);
+
+        builder.Property(po => po.SubTotal)
+            .HasColumnType("decimal(18,4)");
+
+        builder.Property(po => po.TaxAmount)
+            .HasColumnType("decimal(18,4)");
+
+        builder.Property(po => po.TotalAmount)
+            .HasColumnType("decimal(18,4)");
+
+        builder.Property(po => po.Status)
+            .HasConversion<string>()
+            .HasMaxLength(30);
+
+        builder.Property(po => po.Notes)
+            .HasMaxLength(1000);
+
+        builder.Property(po => po.InvoiceReference)
+            .HasMaxLength(100);
+
+        builder.HasIndex(po => po.OrderNumber)
+            .IsUnique()
+            .HasDatabaseName("IX_PurchaseOrders_OrderNumber");
+
+        builder.HasOne(po => po.Supplier)
+            .WithMany(s => s.PurchaseOrders)
+            .HasForeignKey(po => po.SupplierId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(po => po.CreatedByUser)
+            .WithMany(u => u.PurchaseOrders)
+            .HasForeignKey(po => po.CreatedByUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(po => po.Items)
+            .WithOne(i => i.PurchaseOrder)
+            .HasForeignKey(i => i.PurchaseOrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasQueryFilter(po => !po.IsDeleted);
+    }
+}
