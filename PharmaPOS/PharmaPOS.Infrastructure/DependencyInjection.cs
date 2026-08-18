09af9d82f8132f1061b895947a903787d057f6af
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-08-18 07:33:56 +0300

    Added the Dependency Injection file and modified the app settings

---
+++ PharmaPOS/PharmaPOS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PharmaPOS.Infrastructure.Data;
+using PharmaPOS.Infrastructure.Repositories.Implementations;
+using PharmaPOS.Infrastructure.Repositories.Interfaces;
+using PharmaPOS.Infrastructure.Seeders;
+
+namespace PharmaPOS.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Database
+        services.AddDbContext<PharmaPOSDbContext>(options =>
+        {
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"),
+                sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(typeof(PharmaPOSDbContext).Assembly.FullName);
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorNumbersToAdd: null);
+                });
+        });
+
+        // Repositories
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+        // Seeder
+        services.AddScoped<PharmaPOSSeeder>();
+
+        return services;
+    }
+}
--- PharmaPOS/PharmaPOS.Infrastructure/obj/Debug/net10.0/PharmaPOS.Infrastructure.AssemblyInfo.cs
+++ PharmaPOS/PharmaPOS.Infrastructure/obj/Debug/net10.0/PharmaPOS.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmaPOS.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307ce7d6cfab8c29a10a261ce0c06da0873d44a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69a46ea240e2e5c266d281ab1690b9256d360f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmaPOS.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmaPOS.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
